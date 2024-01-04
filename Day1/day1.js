726a7c735cf685318b3354e3a583b190cd6daf4d
bill.bogle <bill.bogle@Bills-MacBook-Pro.local>
2024-01-04 10:32:43 -0800

    Second part of day 1

--- Day1/day1.js
+++ Day1/day1.js
@@ -14,4 +14,29 @@ function first_part() {
   return sum;
 }
 
-console.log(first_part()); 
+function second_part() {
+  const mapper ={
+    'zero': '0',
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+  };
+  let sum = 0;
+  for (let line in input_strings) {
+    line = line.replace(/one|two|three|four|five|six|seven|eight|nine|zero/g, match => mapper[match]);
+    let first_num = line.match(/\d/);
+    let last_num = line.match(/\d(?=[^\d]*$)/);
+    if (first_num == null || last_num == null) continue;
+    let combined = first_num[0] + last_num[0];
+    sum += parseInt(combined);
+  }
+  return sum;
+}
+
+console.log(second_part()); 
